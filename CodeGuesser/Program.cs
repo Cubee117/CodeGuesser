49305710180acea937ccfc9616a33aa054b4694a
Wyatt <wyattsharm44@gmail.com>
2021-10-18 17:22:12 +0100

    version 1.0

---
+++ CodeGuesser/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CodeGuesser
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("{0}@codeguesser:~$ ", Environment.UserName);
+            var fileLocation = Console.ReadLine();
+        }
+    }
+}
