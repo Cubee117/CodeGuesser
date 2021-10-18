6113ae69ecd912ec2dbd6bdad9b290e1424586d0
Wyatt <wyattsharm44@gmail.com>
2021-10-18 21:14:59 +0100

    Very basic command handler

--- CodeGuesser/Handler.cs
+++ CodeGuesser/Handler.cs
@@ -1,8 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CodeGuesser
 {
-    public class Handler
+    public static class Handler
     {
+        public static void Process(string cmd)
+        {
+            var commands = new Dictionary<string, Action<string[]>>()
+            {
+                ["loadfile"] = delegate(string[] strings)
+                {
+                    if (strings.Length < 2) { Console.WriteLine("Not enough arguments!"); return; }
+                    foreach (var character in Helper.LoadFile(strings[1]).SelectMany(code => code))
+                    {
+                        Helper.SendInput(Helper.GetWindow("RustClient"), Convert.ToByte(character), 700);
+                    }
+                }
+            };
+            var args = cmd.Split(' ');
+            if (commands.TryGetValue(args[0], out var command))
+            {
+                command(args);
+            }
+            else
+            {
+                // command not found...
+            }
+        }
     }
 }
