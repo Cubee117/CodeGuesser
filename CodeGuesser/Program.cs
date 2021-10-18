7ebe36b6260a2a093edf02b28f11ce775dfbf3f1
Wyatt <wyattsharm44@gmail.com>
2021-10-18 19:14:10 +0100

    Added support for program arguments

--- CodeGuesser/Program.cs
+++ CodeGuesser/Program.cs
@@ -7,12 +7,27 @@ namespace CodeGuesser
     {
         public static void Main(string[] args)
         {
+            if (args.Length > 0)
+            {
+                Console.WriteLine("Loaded file: {0}", args[0]);
+                Helper.SetForegroundWindow(Helper.GetWindow("RustClient"));
+                foreach (var code in Helper.LoadFile(args[0]))
+                {
+                    foreach (var character in code)
+                    {
+                        Helper.SendInput(Helper.GetWindow("RustClient"), Convert.ToByte(character), 700);
+                    }
+                }
+                return;
+            }
+            
             Helper.Startup();
             while (true)
             { 
                 Console.Write("{0}@codeguesser:~$ ", Environment.UserName);
                 var input = Console.ReadLine();
                 var data = Helper.LoadFile(input);
+                Helper.SetForegroundWindow(Helper.GetWindow("RustClient"));
                 foreach (var code in data)
                 {
                     foreach (var character in code)
@@ -22,5 +37,6 @@ namespace CodeGuesser
                 }
             }
         }
+        
     }
 }
